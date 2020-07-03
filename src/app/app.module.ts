165ea5452692480c38a423d45582e0b28265d89a
AkshitaNirwani <akshitanirwani@gmail.com>
2020-07-03 19:43:22 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './global/header/header.component';
 import {FooterComponent} from './global/footer/footer.component'
+import {CardsComponent} from './cards/cards.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeaderComponent , FooterComponent ],
+  declarations: [ AppComponent, HeaderComponent , FooterComponent ,CardsComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
