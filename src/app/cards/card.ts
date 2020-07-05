e7e1c26e2df2fa2b43b85233cdcb0a3c2535fcb4
AkshitaNirwani <akshitanirwani@gmail.com>
2020-07-05 17:50:04 +0530

    3 files modified

--- src/app/cards/card.ts
+++ src/app/cards/card.ts
@@ -1,5 +1,5 @@
 export interface products{
-  pname: string,
+  logo: string,
   desc : string,
-  imgs : string
+  ver:string
 }
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,36 +12,25 @@ export class CardsComponent{
 
   ngOnInit(){
     this.product = [{
-      pname: "https://www.torryharris.com/modules/mod_product_portal/img/digitmarket.png" ,
+      logo: "https://deplomatic-ui.s3.amazonaws.com/assets/logo/DMAPIM_New_logo.png" ,
       desc:"One platform that lets you optimize, monetize and accelerate your digital transformation initiative. It allows businesses to become platform enablers, grow profitable partnerships and build new channels to market.",
-      imgs : "https://source.unsplash.com/Hx5vtx2e4KQ/1400x900"
+      ver:"v2.0.3"
     },
    
     {
-      pname:"https://www.torryharris.com/modules/mod_product_portal/img/repopro.png",
+      logo:"https://deplomatic-ui.s3.amazonaws.com/assets/logo/repopro.png",
       desc:"An enterprise repository tool to simplify storage & track your assets. Helps enterprises manage their assets through a unified, central repository using automation and a rich visualization of relationships between assets.",
-       imgs:"https://source.unsplash.com/FUohNQatzVs/1400x900"
-      
-    },
-    
-    {
-      pname : "https://www.torryharris.com/modules/mod_product_portal/img/conceirgebank.png",
-      desc:"A marketplace-banking product by Torry Harris that helps banks worldwide to offer innovative new products to customers through an API-driven marketplace solution and a simple user interface.",
-     
-      imgs:"https://source.unsplash.com/ex_p4AaBxbs/1400x900"
-      
+      ver:"v3.8.1"
     },
     {
-      pname : "https://www.torryharris.com/modules/mod_product_portal/img/autostub.png",
+      logo : "https://deplomatic-ui.s3.amazonaws.com/assets/logo/autostub.png",
       desc:"An essential tool to speed up API development. It can significantly reduce your build time by quickly designing, prototyping, documenting and testing APIs.",
-      imgs:"https://source.unsplash.com/xITnxxlzGAE/1400x900"
-      
+      ver:"v2.1"
     },
     {
-      pname : "https://www.torryharris.com/modules/mod_product_portal/img/automaton.png",
+      logo : "https://deplomatic-ui.s3.amazonaws.com/assets/logo/automaton.png",
       desc:"A test automation tool with a visual flow-chart-based design interface. It enables complete test automation of all components of an application - Web, APIs, Logs and Database Processes/Operations.",
-      imgs:"https://source.unsplash.com/LqKhnDzSF-8/1400x900"
-      
+      ver:"v2.5"
     }
     ]
   }
