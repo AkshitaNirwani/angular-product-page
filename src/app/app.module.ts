70916b0032c95128cc123aa8b2ea16f0322edede
AkshitaNirwani <akshitanirwani@gmail.com>
2020-07-06 15:38:08 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './global/header/header.component';
@@ -10,7 +12,11 @@ import {DemoMaterialModule} from './material-module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, DemoMaterialModule ],
+  imports:      [ BrowserModule, 
+  FormsModule, 
+  DemoMaterialModule,
+  BrowserAnimationsModule,
+ ],
   declarations: [ AppComponent, HeaderComponent , FooterComponent ,CardsComponent],
   bootstrap:    [ AppComponent ]
 })
